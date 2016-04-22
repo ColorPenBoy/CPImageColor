7e31cd071349527e223c74fd98b8869ed71ba0fc
张强 <zq252125@163.com>
2016-04-22 15:24:03 +0800

    Get Pixel RGBA

--- CPImageColorDemo/CPImageColorDemo/ViewController.m
+++ CPImageColorDemo/CPImageColorDemo/ViewController.m
@@ -10,14 +10,67 @@
 
 @interface ViewController ()
 
+@property (nonatomic, strong) UIImage * testImage;
+
 @end
 
 @implementation ViewController
 
 - (void)viewDidLoad {
     [super viewDidLoad];
-    // Do any additional setup after loading the view, typically from a nib.
+    
+    self.testImage = [UIImage imageNamed:@"1.jpg"];
+    
+    // 第一步：获取到一张图片中某个Pixel的RGBA值
+    [self getRGBAFromImage:self.testImage atX:0 andY:0];
+
+    
+    
 }
+
+#pragma mark - 获取到一张图片中某个Pixel的RGBA值
+- (void)getRGBAFromImage:(UIImage *)image atX:(int)xx andY:(int)yy {
+    
+    CGImageRef imageRef = [image CGImage];
+    size_t width = CGImageGetWidth(imageRef);
+    size_t height = CGImageGetHeight(imageRef);
+    
+    // 从image的data buffer中取得影像，放入格式化后的rawData中
+    CGColorSpaceRef colorSpace = CGColorSpaceCreateDeviceRGB();
+    unsigned char *rawData = (unsigned char *)malloc(height * width * 4);
+    NSUInteger bytesPerPixel = 4;
+    NSUInteger bytesPerRow = bytesPerPixel * width;
+    NSUInteger bitsPerComponent = 8;
+    CGContextRef context = CGBitmapContextCreate(rawData, width, height, bitsPerComponent, bytesPerRow, colorSpace, kCGImageAlphaPremultipliedLast | kCGBitmapByteOrder32Big);
+    CGColorSpaceRelease(colorSpace);
+    
+    // 清空CGContextRef再绘制
+    CGContextClearRect(context, CGRectMake(0.0, 0.0, width, height));
+    CGContextDrawImage(context, CGRectMake(0, 0, width, height), imageRef);
+    CGContextRelease(context);
+    
+    // 将XY坐标转成一维数组
+    unsigned long byteIndex = (bytesPerRow * yy) + (bytesPerPixel * xx);
+    
+    // 取得RGBA位的数据
+    CGFloat red   = rawData[byteIndex];
+    CGFloat green = rawData[byteIndex + 1];
+    CGFloat blue  = rawData[byteIndex + 2];
+    CGFloat alpha = rawData[byteIndex + 3];
+    
+    // 利用RGB计算灰阶的亮度值
+    CGFloat gray = (red + green + blue)/3 ;
+    
+    // 输出
+    NSLog(@"%@",[NSString stringWithFormat:@"%.2f", red]);
+    NSLog(@"%@",[NSString stringWithFormat:@"%.2f", green]);
+    NSLog(@"%@",[NSString stringWithFormat:@"%.2f", blue]);
+    NSLog(@"%@",[NSString stringWithFormat:@"%.2f", alpha]);
+    NSLog(@"%@",[NSString stringWithFormat:@"%.2f", gray]);
+    
+    free(rawData);
+}
+
 
 - (void)didReceiveMemoryWarning {
     [super didReceiveMemoryWarning];
--- README.md
+++ README.md
@@ -1,1 +1,40 @@
 # CPImageColor
+
+由于业务需求，Core 封装的 SDK是由C++ OpenCV编写的，需要图片转换成BGR格式。
+
+####格式类型：
+
+* RBGA -  包含Alpha通道
+* RGB
+* BGR
+* Gray - 灰度图
+
+首先理解一张图片的组成，每张图片都是由无数个有序排列的带有颜色的像素点（Pixel）组成的，这些Pixel的排列都可以理解为一个二维数组，每个Pixel都是一个颜色点，即包含RGBA的Pixel。
+
+####什么是RGBA呢
+* R - Red
+* G - Green
+* B - Blue
+* A - Alpha 透明度
+
+####什么是Gray灰度
+灰度（Gray scale）数字图像是每个像素只有一个采样颜色的图像。这类图像通常显示为从最暗黑色到最亮的白色的灰度
+
+那么如何获取一个Pixel的Gray value呢：
+
+```
+心理学公式:
+	Gray = R * 0.299 + G * 0.587 + B * 0.114
+
+平均值方法:
+    GRAY = (RED + BLUE + GREEN) / 3
+   （GRAY,GRAY,GRAY ） 替代 （RED,GREEN,BLUE）
+   
+位移算法:
+	Gray = (R*19595 + G*38469 + B*7472) >> 16
+```
+
+计算机上所有的颜色都可以用这4个数值的组合来表示出来，RGB即为计算机的三原色。
+
+####第一步：获取到一张图片中某个Pixel的RGBA值
+
